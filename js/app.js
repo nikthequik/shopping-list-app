963fb01b4ea449f45157f6b67d89fbf05dc82927
nikthequik <quik.the.nick@gmail.com>
2016-05-01 14:26:16 -0600

    -first commit to lab -working on removeItem functionality

--- js/app.js
+++ js/app.js
@@ -1,11 +1,21 @@
 $(function() {
+	var itemNum = 1;
 	$('.addItem').on('click', function() {
 		var input = document.getElementById("inputItem").value;
-		var newItem = $('<h1 class="item"></h1>').text(input);
+		var newItem = $('<h1 class="item" id="item' + itemNum + '"></h1>').text(input);
 		$('.item-box').prepend(newItem);
+		itemNum += 1;
 		document.getElementById("inputItem").value = "Enter another item...";
-		});
+	});
+
 	$('#inputItem').on('focus', function() {
 		document.getElementById("inputItem").value = "";
 	});
+
+	var removeItem = function() {
+		console.log("fire");
+		$(this).remove();
+	};
+
+
 });
