ce97631f3255be0a90d099339a1c124e01ac342a
nikthequik <quik.the.nick@gmail.com>
2016-05-01 12:32:20 -0600

    -remove console log

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 $(function() {
 	$('.addItem').on('click', function() {
 		var input = document.getElementById("inputItem").value;
-		console.log(input);
 		var newItem = $('<h1 class="item"></h1>').text(input);
 		$('.item-box').prepend(newItem);
 		document.getElementById("inputItem").value = "";
