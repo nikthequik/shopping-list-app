c78c96b628feef9a8bf9d721c2602fac5a887977
nikthequik <quik.the.nick@gmail.com>
2016-05-01 12:29:45 -0600

    -add user input functionality -add clear form after submit

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 $(function() {
-
-	
 	$('.addItem').on('click', function() {
-		var newItem = $('<h1 class="item"></h1>').text("Input");
+		var input = document.getElementById("inputItem").value;
+		console.log(input);
+		var newItem = $('<h1 class="item"></h1>').text(input);
 		$('.item-box').prepend(newItem);
+		document.getElementById("inputItem").value = "";
 		});
 });
