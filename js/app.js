686ac9beaa0f082f3eecc749311a60486c2b63db
nikthequik <quik.the.nick@gmail.com>
2016-05-01 22:57:49 -0600

    -position remove button -experiment with remove functionality

--- js/app.js
+++ js/app.js
@@ -3,8 +3,10 @@ $(function() {
 	var itemNum = 1;
 	$('.addItem').on('click', function() {
 		var input = document.getElementById("inputItem").value;
-		var newItem = $('<h1 class="item" id="item' + itemNum + '"></h1><button id="remove" onclick="kill()"></button>').text(input);
+		var newItem = $('<div class="item" id="item' + itemNum + '"><h1></h1></div>').text(input);
+		console.log(newItem);
 		$('.item-box').prepend(newItem);
+		$('#item' + itemNum).append('<button id="remove">Remove</button>');
 		itemNum += 1;
 		document.getElementById("inputItem").value = "Enter another item...";
 	});
@@ -13,8 +15,10 @@ $(function() {
 		document.getElementById("inputItem").value = "";
 	});
 
-	$('.item').on('click')
 
+	$('#remove').on('click', (function () {
+		$(this).parent().remove();
+	}));
 	
 
 
