9825d04ab3cd5cb1e3ca599bb585354d9e307ada
nikthequik <quik.the.nick@gmail.com>
2016-05-02 14:17:36 -0600

    -exchange vanilla js for jquery

--- js/app.js
+++ js/app.js
@@ -2,20 +2,21 @@ $(function() {
 
 	var itemNum = 1;
 	$('.addItem').on('click', function() {
-		var input = document.getElementById("inputItem").value;
+		var input = $('#inputItem').val();
 		console.log(input);
 		if (input != "" && input != "Enter another item..." && input != "Enter an item...") {
 			var newItem = $('<div class="item" id="item' + itemNum + '"><h1></h1></div>').text(input);
 			console.log(newItem);
 			$('.item-box').prepend(newItem);
 			$('#item' + itemNum).append('<button class="remove" onclick="kill(this)">Remove</button>');
+			$('#item' + itemNum).prepend('<input class="check" type="checkbox"></input>');
 			itemNum += 1;
-			document.getElementById("inputItem").value = "Enter another item...";
+			$('#inputItem').val("Enter another item...");
 		}
 	});
 
 	$('#inputItem').on('focus', function() {
-		document.getElementById("inputItem").value = "";
+		$(this).val("");
 	});
 
 
